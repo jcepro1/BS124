488c8109a7ed6f4b149dbe68a1c66a48365f80df
jcepro1 <115606134+jcepro1@users.noreply.github.com>
2024-05-07 08:03:57 -0400

    Add files via upload

---
+++ Examensb/NEnt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examensb
+{
+    class NEnt
+    {
+    
+        private int n;
+
+
+        public NEnt()
+        {
+            n = 0;
+        }
+
+
+        public void Cargar(int dato)
+        {
+            n = dato;
+        }
+
+        public bool verif_fibo()
+        {
+            int a = -1;
+            int b = 1;
+            int c;
+            do
+            {
+                c = checked(a + b);
+                a = b;
+                b = c;
+            }
+            while (!((c == n) || (c > n)));
+            return c == n;
+        }
+    }
+}
